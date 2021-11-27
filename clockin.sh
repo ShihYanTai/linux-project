e45599d08a25ba8dd8c8edce80fb7ab8835e8d07
Humboldt <77721579+ShihYanTai@users.noreply.github.com>
2021-11-27 21:25:14 +0800

    Add files via upload

---
+++ clockin.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# everyday clockin
+links http\:\/\/service\.ispan\.com\.tw\:2330\/api\/class\/remoteAttendance\?qrcode\=eyJjbGFzc0lEIjoiSkpCRFNFVDIxMDciLCJFbWFpbCI6Inlhbi50YWkuc2hpaEBnbWFpbC5jb20iLCJOYW1lIjoi5Y-y5pmP6Ie6IiwgImN1c3RvbWVyU04iOiIyMDA5MTMxNTk5NTYiLCJJc01lYXRMZXNzIjoiTiJ9 | xdotool key q+KP_Enter
+echo "ok!"
